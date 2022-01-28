40444a6d441dd346c44fe56ed4a39dbee99d05c1
Cang <cang.dinh@deutschebahn.com>
2022-01-28 13:14:35 +0100

    Add mvp layout for match table

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,39 @@ import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import './App.css';
 import Foo from './Foo';
-import theme from './theme'
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <TextField 
+          variant="outlined" 
+          color="secondary" 
+          type="date" 
+        />
+        <p>
+          <Button variant="contained" color="primary">
+            Spiel bla 
+          </Button>
+        </p>
         <Stack spacing={2} direction="row">
-        <div> <TextField id="outlined-basic" label="Tean 1" variant="outlined"/>
-          <Foo></Foo>
-        </div>
-        <div> <TextField id="outlined-basic" label="Tean 2" variant="outlined"/>
-          <Foo></Foo>
-        </div>
+          <div> 
+            <Button variant="contained" color="secondary" size="large">
+              Team 1
+            </Button>
+            <p />
+            <TextField id="outlined-basic" label="Tea 2" variant="outlined"/>
+            <Foo></Foo>
+          </div>
+          <div> 
+            <Button variant="contained" color="secondary" size="large">
+              Team 2 
+            </Button>
+            <p />
+              <TextField id="outlined-basic" label="Tea 2" variant="outlined"/>
+            <Foo></Foo>
+          </div>
           {/* <Button variant="text">Text</Button>
           <Button variant="contained">Contained</Button>
           <Button variant="outlined">Bla</Button> */}
